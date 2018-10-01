9e7a5c6aa0b154012786d774ce9c99444c72692c
jadermmoura <jader.mmoura@gmail.com>
2018-09-30 22:58:07 -0300

    versão dia 30

--- src/main/java/br/edu/ifrs/dv/lista3/controle/UsuarioControle.java
+++ src/main/java/br/edu/ifrs/dv/lista3/controle/UsuarioControle.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import br.edu.ifrs.dv.lista3.DAO.UsuarioDAO;
 import br.edu.ifrs.dv.lista3.erros.NaoEncontrado;
+import br.edu.ifrs.dv.lista3.modelo.Telefone;
 import br.edu.ifrs.dv.lista3.modelo.Usuario;
 import java.util.Optional;
 import javax.validation.Valid;
@@ -27,70 +28,86 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping(path = "/api/usuario")
 @Valid
 public class UsuarioControle {
-
+    
     @Autowired
     UsuarioDAO usuarioDAO;
-
+    
     @RequestMapping(path = "/", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Iterable<Usuario> buscar() {
         return usuarioDAO.findAll();
     }
-
+    
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Usuario recuperar(@PathVariable int id) {
         Optional<Usuario> usuarioId = usuarioDAO.findAllById(id);
         if (usuarioId.isPresent()) {
             return usuarioId.get();
-
+            
         } else {
             throw new NaoEncontrado("Id não encontrado");
-
+            
         }
     }
     
-    @RequestMapping(path = "/{id}/telefones", method = RequestMethod.GET)
+    @RequestMapping(path = "/nome/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public Usuario recuperarTelefone(@PathVariable int id) {
-        Optional<Usuario> usuarioId = usuarioDAO.findAllById(id);
-        if (usuarioId.isPresent()) {
-            return usuarioId.get();
-
-        } else {
-            throw new NaoEncontrado("Id não encontrado");
-
-        }
+    public Iterable<Usuario> buscarNome(@PathVariable("nome") String name
+    ) {
+//        if (igual != null) {
+//            return usuarioDAO.findByNome(igual);
+//        } else {
+//
+//            return usuarioDAO.findByNomeContaining(contem);
+//        }
+        return usuarioDAO.findByNome(name);
     }
-
+    
+   @RequestMapping(path = "/{idProduto}/telefones/", 
+            method = RequestMethod.GET)
+    public Iterable<Telefone> listarModelo(@PathVariable int idProduto) {
+        return this.recuperar(idProduto).getTelefones();
+   
+    }
+    
     @RequestMapping(path = "/", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.OK)
-    public Usuario inserir(@RequestBody Usuario usuario) {
-        usuario.setId(0);
-        return usuarioDAO.save(usuario);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario inserir(@RequestBody Usuario usuarioNovo) {
+        usuarioNovo.setId(0);
+        Usuario usuarioSalvo = usuarioDAO.save(usuarioNovo);
+        return usuarioNovo;
     }
-
+    
+    @RequestMapping(path = "/", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void delete() {
+        
+        usuarioDAO.deleteAll();
+        
+    }
+    
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable int id) {
-
+        
         usuarioDAO.deleteById(id);
-
+        
     }
-
+    
     @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.OK)
     public Usuario editar(@PathVariable int id, @RequestBody Usuario usuarioNovo) {
         usuarioNovo.setId(id);
-
+        
         Usuario usuarioAntigo = this.recuperar(id);
         usuarioAntigo.setNome(usuarioNovo.getNome());
         usuarioAntigo.setCpf(usuarioNovo.getCpf());
         usuarioAntigo.setEmail(usuarioNovo.getEmail());
         usuarioAntigo.setTelefones(usuarioNovo.getTelefones());
-
+        
         return usuarioDAO.save(usuarioAntigo);
-
+        
     }
-
+    
 }
